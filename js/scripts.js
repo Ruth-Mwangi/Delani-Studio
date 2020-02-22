fa5ce5d038cb11aac319ab70767f917f3c18aac7
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-02-22 19:11:52 +0300

    Created hover function for the portfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,103 @@ $(document).ready(function(){
         $("#hide-product").fadeToggle();
 
     });
+
+    // portfolio hover
+    $("#portfolio-container img").hover(function(){
+        var x=(this.id);
+    
+
+        switch (x){
+            case "portfolio-1":
+                $("#"+x).hide();
+                $("#portfolio-container #hide-portfolio-1").css("background","url(../images/portfolio/work4.jpg)")
+                $("#hide-portfolio-1").show();
+                
+                break;
+            case "portfolio-2":
+                $("#"+x).hide();
+                $("#portfolio-container #hide-portfolio-2").css("background","url(../images/portfolio/work3.jpg)")
+                $("#hide-portfolio-2").show();
+                break;
+            case "portfolio-3":
+                $("#"+x).hide();
+                $("#portfolio-container #hide-portfolio-3").css("background","url(../images/portfolio/work2.jpg)")
+                $("#hide-portfolio-3").show();
+                break;
+            case "portfolio-4":
+                $("#"+x).hide();
+                $("#portfolio-container #hide-portfolio-4").css("background","url(../images/portfolio/work1.jpg)")
+                $("#hide-portfolio-4").show();
+                break;
+            case "portfolio-5":
+                $("#"+x).hide();
+                $("#portfolio-container #hide-portfolio-5").css("background","url(../images/portfolio/work5.jpg)")
+                $("#hide-portfolio-5").show();
+                break;
+            case "portfolio-6":
+                $("#"+x).hide();
+                $("#portfolio-container #hide-portfolio-6").css("background","url(../images/portfolio/work6.jpg)")
+                $("#hide-portfolio-6").show();
+                break;
+            case "portfolio-7":
+                $("#"+x).hide();
+                $("#portfolio-container #hide-portfolio-7").css("background","url(../images/portfolio/work7.jpg)")
+                $("#hide-portfolio-7").show();
+                break;
+            case "portfolio-8":
+                $("#"+x).hide();
+                $("#portfolio-container #hide-portfolio-8").css("background","url(../images/portfolio/work8.jpg)")
+                $("#hide-portfolio-8").show();
+                break;
+            default:
+                break;
+        }
+    
+    },function(){
+        
+        var x=(this.id);
+        switch (x){
+            case "portfolio-1":
+                $("#"+x).show();
+                $("#hide-portfolio-1").hide();
+                
+                break;
+            case "portfolio-2":
+                $("#"+x).show();
+                $("#hide-portfolio-2").hide();
+                break;
+            case "portfolio-3":
+                $("#"+x).show();
+                $("#hide-portfolio-3").hide();
+                
+                break;
+            case "portfolio-4":
+                $("#"+x).show();
+                $("#hide-portfolio-4").hide();
+                break;
+            case "portfolio-5":
+                $("#"+x).show();
+                $("#hide-portfolio-5").hide();
+                
+                break;
+            case "portfolio-6":
+                $("#"+x).show();
+                $("#hide-portfolio-6").hide();
+                break;
+            case "portfolio-7":
+                $("#"+x).show();
+                $("#hide-portfolio-7").hide();
+                
+                break;
+            case "portfolio-8":
+                $("#"+x).show();
+                $("#hide-portfolio-8").hide();
+                break;
+            default:
+                break;
+        }
+        
+    });
     
 
     
