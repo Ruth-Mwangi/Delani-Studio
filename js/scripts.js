2772f0621fc64aee11e348c58a6be53233f8ead9
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-02-22 19:10:24 +0300

    Created click function for the development image

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+    //what we do
+    $(".design").click(function(){
+        $("#show-design").fadeToggle();
+        $("#hide-design").fadeToggle();
+
+    });
+    $(".development").click(function(){
+        $("#show-development").fadeToggle();
+        $("#hide-development").fadeToggle();
+
+    });
     
 
     
